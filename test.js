c5b8ab197ea675eb6396f1bb8156742beb28f237
Mate <iviatecpe@gmail.com>
2021-08-28 22:34:15 +0700

    fix

--- test.js
+++ test.js
@@ -4,77 +4,53 @@ var qs = require('qs');
 const puppeteer = require("puppeteer");
 const timeout = 30000
 var request = require('request');
-var options = {
-  'method': 'POST',
-  'url': 'https://imba69.com/users/sign_in',
-  formData: {
-    'user[username]': '0643258932',
-    'user[password]': 'ฤฟ696969'
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.headers["set-cookie"]);
-  var data = new FormData();
-  data.append('user[username]', '0643258932');
-  data.append('user[password]', 'ฤฟ696969');
 
-  var config = {
-    method: 'post',
-    url: 'https://imba69.com/users/sign_in',
-    headers: {
-      'Cookie': response.headers["set-cookie"].join(),
-      ...data.getHeaders()
-    },
-    data: data
+async function test() {
+  var options = {
+    'method': 'POST',
+    'url': 'https://imba69.com/users/sign_in',
+    formData: {
+      'user[username]': '0643258932',
+      'user[password]': 'ฤฟ696969'
+    }
   };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.headers["set-cookie"]);
+    var data = new FormData();
+    data.append('user[username]', '0643258932');
+    data.append('user[password]', 'ฤฟ696969');
 
-  axios(config)
-    .then(function (response) {
-      console.log(response.headers['set-cookie']);
-      var config = {
-        method: 'get',
-        url: 'https://imba69.com/member/gamelink?vendor=sexy&game_id=undefined&game_code=undefined&mobile=false',
-        headers: {
-          'Cookie': response.headers['set-cookie'].join()
+    var config = {
+      method: 'post',
+      url: 'https://imba69.com/users/sign_in',
+      headers: {
+        'Cookie': response.headers["set-cookie"].join(),
+        ...data.getHeaders()
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(async function (response) {
+        console.log(response.headers['set-cookie']);
+        let token = "7qVRu7b77Vc"
+        let walletAPI = `https://imba69.com/member/get_credit_limit?token=${token}`
+        let config = {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': response.headers['set-cookie'].join()
+          }
         }
-      };
-
-      axios(config)
-        .then(async function (response) {
-          console.log(response.data.data);
-
-          const browser = await puppeteer.launch({
-            headless: true,
-            devtools: false,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-          });
-
-          const page2 = await browser.newPage();
-          // await page2.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-          page2.setDefaultTimeout(timeout)
-          await page2.goto(response.data.data, {
-            waitUntil: "networkidle2"
-          });
-
-          await page2.waitForSelector('#playerInfo')
-          const cookies = await page2.cookies()
-          // console.log(cookies)
-
-          let cookieHeader2 = ''
-          cookies.forEach((value) => {
-            cookieHeader2 += value.name + '=' + value.value + '; '
-          })
+        let res = await axios.get(walletAPI, config)
+        console.log(res.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+}
 
 
-          console.log(cookieHeader2)
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+
 
--- walletWorker.js
+++ walletWorker.js
@@ -3,26 +3,92 @@
 require('log-timestamp');
 const { parentPort, workerData } = require('worker_threads');
 const axios = require('axios');
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 const utils = require("./utils.js")
 const moment = require('moment-timezone');
 const db = require("./app/models");
+var request = require('request');
+var FormData = require('form-data');
 var userData = null
-var browser = null
-var page = null
+// var browser = null
+// var page = null
 var imbaCookie = ""
 registerForEventListening();
 
+async function test(username, password, token) {
+    if (userData.is_mock) {
+        const user = await db.user.findOne({
+            where: {
+                id: userData.id,
+            },
+        })
+        parentPort.postMessage({ action: 'credit', data: { userId: userData.id, wallet: parseFloat(user.mock_wallet).toFixed(2) } })
+    }
+    else {
+        var options = {
+            'method': 'POST',
+            'url': 'https://imba69.com/users/sign_in',
+            formData: {
+                'user[username]': username,
+                'user[password]': password
+            }
+        };
+        request(options, function (error, response) {
+            if (error) return null;
+            // console.log(response.headers["set-cookie"]);
+            var data = new FormData();
+            data.append('user[username]', username);
+            data.append('user[password]', password);
+
+            var config = {
+                method: 'post',
+                url: 'https://imba69.com/users/sign_in',
+                headers: {
+                    'Cookie': response.headers["set-cookie"].join(),
+                    ...data.getHeaders()
+                },
+                data: data
+            };
+
+            axios(config)
+                .then(async function (response) {
+                    // console.log(response.headers['set-cookie']);
+                    let walletAPI = `https://imba69.com/member/get_credit_limit?token=${token}`
+                    let config = {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Cookie': response.headers['set-cookie'].join()
+                        }
+                    }
+                    let res = await axios.get(walletAPI, config)
+                    // console.log(res.data)
+                    if (res.data.success == true) {
+                        parentPort.postMessage({ action: 'credit', data: { userId: userData.id, wallet: parseFloat(res.data.credit).toFixed(2) } })
+
+                    } else {
+                        parentPort.postMessage({ action: 'credit', data: { userId: userData.id, wallet: null } })
+                    }
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    parentPort.postMessage({ action: 'credit', data: { userId: userData.id, wallet: null } })
+                });
+        });
+    }
+
+}
+
 async function getUserImbaWallet(username, password, token) {
     console.log('userData.is_mock <<< ', userData.is_mock)
-    if(userData.is_mock){
+    if (userData.is_mock) {
         const user = await db.user.findOne({
             where: {
                 id: userData.id,
             },
         })
         return user.mock_wallet
-    }else{
+    } else {
         let walletAPI = `https://imba69.com/member/get_credit_limit?token=${token}`
         let config = {
             headers: {
@@ -37,9 +103,9 @@ async function getUserImbaWallet(username, password, token) {
             console.log(res.data)
             return res.data.credit
         }
-        else{
+        else {
             try {
-                if(page){
+                if (page) {
                     await page.close()
                 }
                 page = await browser.newPage();
@@ -48,17 +114,17 @@ async function getUserImbaWallet(username, password, token) {
                 await page.goto('https://imba69.com/users/sign_in', {
                     waitUntil: "networkidle2"
                 });
-        
+
                 await page.waitForSelector('input[name="user[username]"]')
                 await page.type('input[name="user[username]"]', username);
                 await page.type('input[name="user[password]"]', password);
-        
+
                 await Promise.all([
                     page.click('button[type="submit"]'),
                     page.waitForNavigation({ waitUntil: 'networkidle0' }),
                 ]);
                 await page.waitForSelector('.img-shield-sys')
-        
+
                 const cookiesImba = await page.cookies()
                 // console.log(cookiesImba)
                 imbaCookie = ""
@@ -88,31 +154,30 @@ async function getUserImbaWallet(username, password, token) {
             }
         }
     }
-    
-    
+
+
 }
 
 
 async function registerForEventListening() {
 
     userData = workerData.user
-    console.log(userData)
-    browser = await puppeteer.launch({
-        headless: true,
-        devtools: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    let wallet = await getUserImbaWallet(userData.username, userData.password, userData.token)
-    parentPort.postMessage({ action: 'credit', data : {userId: userData.id, wallet: parseFloat(wallet).toFixed(2)}})
+    // console.log(userData)
+    // browser = await puppeteer.launch({
+    //     headless: true,
+    //     devtools: false,
+    //     args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // });
+    test(userData.username, userData.password, userData.token)
+
 
     // callback method is defined to receive data from main thread
-    let cb = async  (err, result) => {
+    let cb = async (err, result) => {
         if (err) return console.error(err);
         // console.log("Thread id ")
-        
+
         if (result.action == 'wallet') {
-            let wallet = await getUserImbaWallet(userData.username, userData.password, userData.token)
-            parentPort.postMessage({ action: 'credit', data : {userId: userData.id, wallet: parseFloat(wallet).toFixed(2)}})
+            test(userData.username, userData.password, userData.token)
         } else if (result.action == 'play') {
             // console.log(`Thred id ${workerData.id} action ${result.action}`)
             isPlay = true
