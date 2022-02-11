860cf256537395adcd19a0f7284a0bd628f41c39
schewti <tenebrous.schewti@gmail.com>
2022-02-11 23:52:07 +0700

    feat(util): add get credit util

--- example.js
+++ example.js
@@ -1,10 +1,15 @@
-const { reCookie } = require('./utilities');
+const { getCredit, reCookie } = require('./utilities');
 
 async function example() {
   const username = 'username';
   const password = 'password';
   const cookie = await reCookie(username, password);
+
+  const userToken = 'token';
+  const creditResponse = await getCredit(userToken);
+
   console.log(cookie);
+  console.log(creditResponse); // { success: true, credit: 0.14 }
 }
 
 example();
--- utilities/index.js
+++ utilities/index.js
@@ -1,7 +1,9 @@
 const cookie = require('./cookie');
+const credit = require('./credit');
 const login = require('./login');
 
 module.exports = {
   ...cookie,
+  ...credit,
   ...login,
 };
