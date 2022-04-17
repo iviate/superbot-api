b141f29dfddc1f2bea2db999818680868b91ce90
schewti <tenebrous.schewti@gmail.com>
2022-04-17 21:03:03 +0700

    build(config): update to use env

--- config/bot.config.js
+++ config/bot.config.js
@@ -1,13 +1,46 @@
-module.exports = {
-    user: {
-        1: { username : "0895847545", pwd : "SADok:kl"},
-        2: { username : "0895847595", pwd : "SADok:kl"},
-        3: { username : "0895847842", pwd : "SADok:kl"},
-        4: { username : "0895847261", pwd : "SADok:kl"},
-        5: { username : "0895847111", pwd : "SADok:kl"},
-        6: { username : "0895847112", pwd : "SADok:kl"},
-        31: { username : "0895848798", pwd : "SADok:kl"},
-        32: { username : "0895848878", pwd : "SADok:kl"},
-        71: { username : "0895841145", pwd : "SADok:kl"}
-    }
+let user = {};
+try {
+  user = JSON.parse(process.env.BOT_USER);
+} catch (error) {
+  user = {
+    1: {
+      username: process.env.BOT_USER_1 || '0899999901',
+      pwd: process.env.BOT_PASSWORD_1 || '1111',
+    },
+    2: {
+      username: process.env.BOT_USER_2 || '0899999902',
+      pwd: process.env.BOT_PASSWORD_2 || '2222',
+    },
+    3: {
+      username: process.env.BOT_USER_3 || '0899999903',
+      pwd: process.env.BOT_PASSWORD_3 || '3333',
+    },
+    4: {
+      username: process.env.BOT_USER_4 || '0899999904',
+      pwd: process.env.BOT_PASSWORD_4 || '4444',
+    },
+    5: {
+      username: process.env.BOT_USER_5 || '0899999905',
+      pwd: process.env.BOT_PASSWORD_5 || '5555',
+    },
+    6: {
+      username: process.env.BOT_USER_6 || '0899999906',
+      pwd: process.env.BOT_PASSWORD_6 || '6666',
+    },
+    31: {
+      username: process.env.BOT_USER_31 || '0899999907',
+      pwd: process.env.BOT_PASSWORD_31 || '7777',
+    },
+    32: {
+      username: process.env.BOT_USER_32 || '0899999908',
+      pwd: process.env.BOT_PASSWORD_32 || '8888',
+    },
+    71: {
+      username: process.env.BOT_USER_71 || '0899999909',
+      pwd: process.env.BOT_PASSWORD_71 || '9999',
+    },
+  };
 }
+module.exports = {
+  user,
+};
--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    DB: "ibot",
-    dialect: "mysql",
-  };
+  HOST: process.env.DB_HOST || 'localhost',
+  USER: process.env.DB_USER || 'root',
+  DB: process.env.DB_DATABASE || 'ibot',
+  PASSWORD: process.env.DB_PASSWORD || 'ibot',
+  dialect: 'mysql',
+};
