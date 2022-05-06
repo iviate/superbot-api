bc667ba21ee54d0ae5bdaa7568473d9f9b0bdb66
schewti <tenebrous.schewti@gmail.com>
2022-05-06 22:21:15 +0700

    feat(): add axios proxy

---
+++ addProxy.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+axios.interceptors.request.use((config) => {
+  if (process.env.IS_ENABLE_PROXY) {
+    return {
+      ...config,
+      proxy: {
+        host: process.env.PROXY_HOST,
+        port: process.env.PROXY_PORT,
+        auth: {
+          username: process.env.PROXY_USER,
+          password: process.env.PROXY_PASSWORD,
+        },
+      },
+    };
+  }
+
+  return config;
+});
+
+module.exports = axios;
