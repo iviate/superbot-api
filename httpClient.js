9a566c2b70cda4124eff5e0086e7b92fe5fe8b75
schewti <tenebrous.schewti@gmail.com>
2022-05-12 20:46:40 +0700

    feat(): add retry when 403

--- httpClient.js
+++ httpClient.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 
 const httpClient = axios.create();
 
+const MAX_RETRY = 3;
+
 httpClient.interceptors.request.use((config) => {
   if (process.env.IS_ENABLE_PROXY === 'true') {
     return {
@@ -19,5 +21,26 @@ httpClient.interceptors.request.use((config) => {
 
   return config;
 });
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const status = error.response ? error.response.status : null;
+
+    if (status === 403) {
+      error.config.__retryCount = error.config.__retryCount || 0;
+
+      if (error.config.__retryCount < MAX_RETRY) {
+        error.config.__retryCount += 1;
+
+        await sleep(100);
+
+        return httpClient(error.config);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 module.exports = httpClient;
