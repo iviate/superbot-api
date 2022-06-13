0f66f41b4de979386926b18f29ccf72b7ce4ca85
schewti <tenebrous.schewti@gmail.com>
2022-06-13 22:08:31 +0700

    feat(): add support DB PORT

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,10 @@
-const dbConfig = require("../../config/db.config.js");
+const dbConfig = require('../../config/db.config.js');
 
-const Sequelize = require("sequelize");
-const { DataTypes } = require("sequelize");
+const Sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  port: 3306,
+  port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   // dialectOptions: {
   //   ssl: {
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   //   }
   // },
   operatorsAliases: 0,
-  logging: false
+  logging: false,
 });
 
 const db = {};
@@ -22,46 +22,68 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("./user.model.js")(sequelize, Sequelize, DataTypes);
-db.bot = require("./bot.model.js")(sequelize, Sequelize, DataTypes);
-db.botTransction = require("./bot.transaction.model.js")(sequelize, Sequelize, DataTypes);
-db.userTransaction = require("./user_transaction.model.js")(sequelize, Sequelize, DataTypes);
-db.wallet_transfer = require("./wallet_transfer.model.js")(sequelize, Sequelize, DataTypes);
-db.member = require("./member.model.js")(sequelize, Sequelize, DataTypes)
-db.member_record = require("./member_record.model.js")(sequelize, Sequelize, DataTypes)
-db.agent_record = require("./agent_record.model.js")(sequelize, Sequelize, DataTypes)
-db.rolling = require("./rolling.model")(sequelize, Sequelize, DataTypes)
-db.rolling_withdraw = require("./rolling_withdraw.model")(sequelize, Sequelize, DataTypes)
-db.mockUserTransaction = require("./mock_user_transaction.model")(sequelize, Sequelize, DataTypes)
+db.user = require('./user.model.js')(sequelize, Sequelize, DataTypes);
+db.bot = require('./bot.model.js')(sequelize, Sequelize, DataTypes);
+db.botTransction = require('./bot.transaction.model.js')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.userTransaction = require('./user_transaction.model.js')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.wallet_transfer = require('./wallet_transfer.model.js')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.member = require('./member.model.js')(sequelize, Sequelize, DataTypes);
+db.member_record = require('./member_record.model.js')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.agent_record = require('./agent_record.model.js')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.rolling = require('./rolling.model')(sequelize, Sequelize, DataTypes);
+db.rolling_withdraw = require('./rolling_withdraw.model')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
+db.mockUserTransaction = require('./mock_user_transaction.model')(
+  sequelize,
+  Sequelize,
+  DataTypes
+);
 
-db.user.hasMany(db.bot, { as: "bots" });
+db.user.hasMany(db.bot, { as: 'bots' });
 db.bot.belongsTo(db.user, {
-  foreignKey: "userId",
-  as: "user",
+  foreignKey: 'userId',
+  as: 'user',
 });
 
-db.bot.hasMany(db.userTransaction, { as: "transactions" });
+db.bot.hasMany(db.userTransaction, { as: 'transactions' });
 db.userTransaction.belongsTo(db.bot, {
-  foreignKey: "botId",
-  as: "bot",
+  foreignKey: 'botId',
+  as: 'bot',
 });
 
-db.botTransction.hasMany(db.userTransaction, { as: "users" });
+db.botTransction.hasMany(db.userTransaction, { as: 'users' });
 db.userTransaction.belongsTo(db.botTransction, {
-  foreignKey: "botTransactionId",
-  as: "bot_transaction",
+  foreignKey: 'botTransactionId',
+  as: 'bot_transaction',
 });
 
-db.bot.hasMany(db.wallet_transfer, {as: "profit_transfer"});
+db.bot.hasMany(db.wallet_transfer, { as: 'profit_transfer' });
 db.wallet_transfer.belongsTo(db.bot, {
-  foreignKey: "botId",
-  as: "bot"
-})
-
-
-
-
-
-
+  foreignKey: 'botId',
+  as: 'bot',
+});
 
 module.exports = db;
--- config/db.config.js
+++ config/db.config.js
@@ -2,6 +2,7 @@ module.exports = {
   HOST: process.env.DB_HOST || 'localhost',
   USER: process.env.DB_USER || 'root',
   DB: process.env.DB_DATABASE || 'ibot',
+  PORT: process.env.DB_PORT || 3306,
   PASSWORD: process.env.DB_PASSWORD || 'ibot',
   dialect: 'mysql',
 };
