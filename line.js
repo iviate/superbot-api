30f720fd23c464d643bb5c4d06902e0ec4abb555
schewti <tenebrous.schewti@gmail.com>
2023-04-07 19:58:02 +0700

    add verify jwt token

--- line.js
+++ line.js
@@ -4,8 +4,30 @@ const db = require('./app/models');
 
 const ACTIVE_IN_MINUTES = 5;
 
+const authenticateJwtToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'JWT token is missing' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'JWT token is invalid' });
+        }
+
+        if (decoded.role !== 'admin') {
+            return res.status(403).json({ message: 'User ID is not allowed' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
+
 function setUpLineRoute(app) {
-    app.get('/status', async function (req, res) {
+    app.get('/status', authenticateJwtToken, async function (req, res) {
         const { isWorking, lastedActive } = await checkIsBotWorking();
 
         res.json({
